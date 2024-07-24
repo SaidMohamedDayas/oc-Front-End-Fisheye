158c6fb8cd87507d9268b9b0c85bccf78d7bdfa2
SaidMohamedDayas <113367982+SaidMohamedDayas@users.noreply.github.com>
2024-07-24 16:45:43 +0200

    feat: complete website

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,56 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+async function getPhotographers() {
+  try {
+    // Récupérer les données des photographes
+    const data = await fetch("../data/photographers.json");
+    if (!data.ok) {
+      throw new Error("HTTP Error! Status : " + data.status);
     }
+    // Récupérer les photographes
+    const photographersData = await data.json();
+    return { photographers: photographersData.photographers };
+  } catch (error) {
+    console.error("Error fetch photographers: ", error);
+    return { photographers: [] };
+  }
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
+// 1- Créer une fonction displayData pour afficher les photographes
+async function displayData(photographers) {
+  // Récupérer la section des photographes
+  const photographersSection = document.querySelector(".photographer_section");
+  if (!photographersSection) {
+    console.error("No photographer section found!");
+    return;
+  }
+  // Récupérer le template des photographes
+  photographers.forEach((photographer) => {
+    // Créer un modèle de photographe
+    const photographerModel = photographerTemplate(photographer);
+    if (photographerModel) {
+      // Récupérer le DOM du photographe
+      const userCardDOM = photographerModel.getUserCardDOM();
+      if (userCardDOM) {
+        // Ajouter le DOM du photographe à la section des photographes
+        photographersSection.appendChild(userCardDOM);
+      } else {
+        console.error("No user card DOM found!");
+      }
+    } else {
+      console.error("No user card model found!");
     }
+  });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+// 2- Ajouter la fonction init pour appeler getPhotographers et displayData
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  if (photographers.length > 0) {
+    displayData(photographers);
+  } else {
+    console.error("No photographers data found to display!");
+  }
+}
+
+// 3- Appeler la fonction init pour lancer l'application
+init();
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,209 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+// Récupérer le nom du photographe dans data.json
+async function getPhotographerName() {
+  try {
+    // Récupérer l'ID du photographe depuis l'URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const photographerId = urlParams.get("id");
+
+    // Récupérer les données des photographes
+    const response = await fetch("data/photographers.json");
+    const { photographers } = await response.json();
+
+    // Trouver le photographe par son ID
+    const photographer = photographers.find(
+      (p) => p.id === parseInt(photographerId)
+    );
+
+    // Renvoyer le nom du photographe, ou une valeur par défaut si non trouvé
+    return photographer ? photographer.name : "Photographe Inconnu";
+  } catch (error) {
+    console.error("Erreur lors de la récupération du nom: ", error);
+    return "Photographe Inconnu"; // Renvoyer un nom par défaut en cas d'erreur
+  }
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+// Fonction pour afficher le modal de contact
+function displayModal() {
+  // Récupérer le backdrop
+  const backdropModal = document.getElementById("backdrop_modal");
+  // Récupérer le modal
+  const modal = document.getElementById("contact_modal");
+  // Afficher le modal
+  modal.style.display = "block";
+  // Afficher le backdrop
+  backdropModal.style.display = "block";
 }
+
+// Fonction pour créer le formulaire de contact
+async function createContactForm(photographerId) {
+  try {
+    // Récupérer le nom du photographe
+    const name = await getPhotographerName(photographerId);
+
+    const backdropModal = document.createElement("div");
+    backdropModal.id = "backdrop_modal";
+    backdropModal.style.display = "none";
+    document.body.appendChild(backdropModal);
+
+    // Créer le div principal et définir son ID et son style
+    const contactModal = document.createElement("div");
+    contactModal.id = "contact_modal";
+    contactModal.setAttribute("role", "dialog");
+    contactModal.setAttribute("aria-labelledby", "contact_modal");
+    contactModal.style.display = "none";
+
+    // Créer le div interne et ajouter la classe 'modal'
+    const modal = document.createElement("div");
+    modal.className = "modal";
+
+    // Créer le header
+    const header = document.createElement("header");
+    header.className = "modal_header";
+
+    // Ajouter le titre du header
+    const h1 = document.createElement("h1");
+    h1.id = "photographer-name";
+    h1.setAttribute("tabindex", "0");
+    h1.appendChild(document.createTextNode("Contactez-moi"));
+
+    // Ajouter un br entre le titre et le nom du photographe
+    const br = document.createElement("br");
+    h1.appendChild(br);
+
+    // Ajouter le nom du photographe
+    h1.appendChild(document.createTextNode(name));
+
+    // Ajouter l'image pour fermer le modal et ajouter un événement onclick
+    const closeBtn = document.createElement("img");
+    closeBtn.src = "assets/icons/close.svg";
+    closeBtn.setAttribute("aria-label", "Close Contact form");
+    closeBtn.onclick = closeModal;
+
+    // Ajouter un écouteur d'événement à la touche Echap
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        closeModal();
+      }
+    });
+
+    // Assembler le header
+    header.appendChild(h1);
+    header.appendChild(closeBtn);
+
+    // Créer le formulaire
+    const form = document.createElement("form");
+    // form.onsubmit = sendForm;
+
+    // Création et ajout du champ 'Prénom'
+    createInputField(form, "firstname", "Prénom", "First name", 3);
+    // Création et ajout du champ 'Nom'
+    createInputField(form, "lastname", "Nom", "Last name", 5);
+    // Création et ajout du champ 'Email'
+    createInputField(form, "email", "Email", "Email", 7);
+    // Création et ajout du champ 'Votre message'
+    createTextAreaField(form, "message", "Votre message", "Your message", 9);
+
+    // Créer le bouton d'envoi
+    const sendBtn = document.createElement("button");
+    sendBtn.className = "contact_button";
+    sendBtn.textContent = "Envoyer";
+    sendBtn.type = "submit";
+    sendBtn.setAttribute("aria-label", "Send"); // Ajouter un attribut aria-label
+    sendBtn.onclick = sendForm;
+
+    // Ajouter le bouton d'envoi au formulaire
+
+    // Assembler le modal complet
+    modal.appendChild(header);
+    modal.appendChild(form);
+    form.appendChild(sendBtn);
+
+    // Ajouter le modal au div principal
+    contactModal.appendChild(modal);
+
+    // Ajouter le div principal au corps du document
+    backdropModal.appendChild(contactModal);
+  } catch (error) {
+    console.error(
+      "Erreur lors de la création du formulaire de contact: ",
+      error
+    );
+  }
+}
+
+// Fonction pour créer un champ de saisie
+function createInputField(form, id, labelText, ariaLabel, labelNum) {
+  const div = document.createElement("div");
+  const label = document.createElement("label");
+  label.textContent = labelText;
+  label.id = `label_for_${id}`; // Id pour aria-labelledby
+  const input = document.createElement("input");
+  input.type = "text";
+  input.name = id;
+  input.id = id;
+  input.setAttribute("aria-labelledby", label.id);
+  div.appendChild(label);
+  div.appendChild(input);
+  form.appendChild(div);
+}
+
+// Fonction pour créer un champ de texte étendu
+function createTextAreaField(form, id, labelText, ariaLabel, labelNum) {
+  const div = document.createElement("div");
+  const label = document.createElement("label");
+  label.textContent = labelText;
+  label.id = `label_for_${id}`;
+  const textarea = document.createElement("textarea");
+  textarea.name = id;
+  textarea.id = id;
+  textarea.setAttribute("aria-labelledby", label.id);
+  div.appendChild(label);
+  div.appendChild(textarea);
+  form.appendChild(div);
+}
+
+// Fonction pour envoyer le formulaire
+async function sendForm(event) {
+  // Empêcher le rechargement de la page
+  event.preventDefault();
+  // Récupérer les valeurs des inputs
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // Si les champs ne sont pas remplis, afficher un message d'erreur
+  if (!firstname || !lastname || !email || !message) {
+    // Afficher un message d'erreur
+    alert("Veuillez remplir tous les champs");
+    return;
+  } else {
+    // Afficher un message de succès
+    alert("Votre message a bien été envoyé !");
+    // Afficher les valeurs en console
+    console.log("Prénom : ", firstname);
+    console.log("Nom : ", lastname);
+    console.log("Email : ", email);
+    console.log("Message : ", message);
+  }
+
+  // Vider les champs du formulaire
+  document.getElementById("firstname").value = "";
+  document.getElementById("lastname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+
+  // Fermer le modal
+  closeModal();
+}
+
+// Fonction pour fermer le modal
+function closeModal() {
+  const modal = document.getElementById("contact_modal");
+  const backdropModal = document.getElementById("backdrop_modal");
+  modal.style.display = "none";
+  backdropModal.style.display = "none";
+}
+
+// Appeler la fonction pour créer le formulaire de contact
+createContactForm();
